8d53a62b89a2dcf3025f6813a3af0cb41dd364b3
Lance <lance@ampostech.com>
2020-03-03 18:05:54 +0800

    save before leaving office

--- python/easy/linked-list-cycle.py
+++ python/easy/linked-list-cycle.py
@@ -7,10 +7,11 @@ Memory Usage: 15.9 MB, less than 100.00% of Python3 online submissions for Linke
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
